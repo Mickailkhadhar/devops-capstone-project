b319d17ce5feb49d4b20a825c93c3e275f5d21cc
Mickailkhadhar <micka.khadhar@gmail.com>
2022-11-23 18:04:46 +0100

    Update routes.py nemove unnecessary spaces

--- service/routes.py
+++ service/routes.py
@@ -66,11 +66,9 @@ def create_accounts():
 def list_accounts():
     """List all accounts """
     app.logger.info("Request to list Accounts")
-
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
-    
     return jsonify(account_list), status.HTTP_200_OK
 
 
@@ -84,13 +82,11 @@ def get_account(account_id):
     This endpoint will read an Account based on the account_id that is requested
     """
     # ... place you code here to READ an account ...
-
     app.logger.info("Request to read an Account with id: %s", account_id)
 
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
-
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -117,13 +113,10 @@ def update_accounts(account_id):
 @app.route("/accounts/<int:account_id>", methods = ["DELETE"])
 def delete_accounts(account_id):
     """ DElete an Account"""
-
     app.logger.info("Request to delete an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if account:
         account.delete()
-
     return "", status.HTTP_204_NO_CONTENT
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
