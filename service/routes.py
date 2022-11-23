45587e41f5df70ae061545cdc318205b513b0357
Mickailkhadhar <micka.khadhar@gmail.com>
2022-11-23 18:11:36 +0100

    Add backspaces

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -86,6 +87,8 @@ def get_account(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
     return account.serialize(), status.HTTP_200_OK
+
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -99,6 +102,8 @@ def update_accounts(account_id):
     account = account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
+
 # ... place you code here to UPDATE an account ...
 ######################################################################
 # DELETE AN ACCOUNT
@@ -112,6 +117,8 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
